286efa9cbcc04d15e2b0401ed666f7ce7a1a8529
goldcollar95 <yjs7298@naver.com>
2022-12-11 18:32:28 +0900

    feat(bridge): BridgeGame을 담당하는 move, retry 1차 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -5,19 +5,38 @@ package bridge.domain;
  */
 public class BridgeGame {
 
+    private static final String Upper = "U";
+    private static final String Correct = "O";
+    private static final String Lower = "D";
+    private static final String NotCorrect = "X";
+    private static int getCount = 1;
+
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public void move(String direction) {
+        if(direction.equals(Upper)){
+            direction = Correct;
+        }
+        if(direction.equals(Lower)){
+            direction = NotCorrect;
+        }
     }
+
+
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
      * <p>
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void retry() {
+    public boolean retry(String direction) {
+        if(direction.contains("R")){
+            getCount++;
+            return true;
+        }
+        return false;
     }
 }
