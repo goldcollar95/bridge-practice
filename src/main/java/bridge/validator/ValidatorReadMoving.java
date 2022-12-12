774cab6f7303f5cdd7e8b164e3b1d66af396ac2d
goldcollar95 <yjs7298@naver.com>
2022-12-12 14:48:23 +0900

    feat(bridge): 다리 방향 유효성검사

--- src/main/java/bridge/validator/ValidatorReadMoving.java
+++ src/main/java/bridge/validator/ValidatorReadMoving.java
@@ -1,4 +1,16 @@
 package bridge.validator;
 
+import bridge.view.ErrorMessage;
+
 public class ValidatorReadMoving {
+
+    public static void checkList(String inputMove){
+        checkDirection(inputMove);
+    }
+
+    public static void checkDirection(String inputMove){
+        if(!inputMove.equals("U") && !inputMove.equals("D")){
+            throw new IllegalArgumentException(ErrorMessage.ERROR_MOVE_DIRECTION.geterrorMessage());
+        }
+    }
 }
