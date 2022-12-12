a49faf0bd5561bf9e3574c44d7e1bfa27e815922
goldcollar95 <yjs7298@naver.com>
2022-12-12 11:21:53 +0900

    feat(brdige): size 예외처리

--- src/main/java/bridge/validator/ValidatorSize.java
+++ src/main/java/bridge/validator/ValidatorSize.java
@@ -1,4 +1,26 @@
 package bridge.validator;
 
+import bridge.view.ErrorMessage;
+
 public class ValidatorSize {
+
+
+    public static void checkList(String sizeInput){
+        checkSize(sizeInput);
+        checkNumber(sizeInput);
+    }
+
+    public static void checkSize(String sizeInput){
+        int size = Integer.parseInt(sizeInput);
+            if(size < 3 || size > 20){
+                throw new IllegalArgumentException(ErrorMessage.ERROR_GAME_RANGE.geterrorMessage());
+            }
+        }
+
+    public static void checkNumber(String sizeInput){
+        String regex = "^[0-9]*$";
+        if(!sizeInput.matches(regex)){
+            throw new IllegalArgumentException(ErrorMessage.ERROR_ONLY_NUMBER.geterrorMessage());
+        }
+    }
 }
--- src/main/java/bridge/view/ErrorMessage.java
+++ src/main/java/bridge/view/ErrorMessage.java
@@ -4,6 +4,7 @@ public enum ErrorMessage {
     ERROR_GAME_RANGE("[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다."),
     ERROR_MOVE_DIRECTION("[ERROR] U와 D이외에 들어올수 없습니다."),
     ERROR_RESTART_BUTTON("[ERROR] R와 Q이외에 들어올수 없습니다."),
+    ERROR_ONLY_NUMBER("[ERROR] 슷자만 가능합니다."),
     ERROR_ISNOT_EMPTY("[ERROR] 빈칸은 들어올 수 없습니다.");
 
     private final String errorMessage;
