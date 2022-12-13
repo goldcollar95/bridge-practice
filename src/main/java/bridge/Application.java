875ed4e44b067fa12862ab1c19c7cae2c5e80082
goldcollar95 <yjs7298@naver.com>
2022-12-13 19:41:52 +0900

    feat(bridge): Application 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,15 @@
 package bridge;
 
+import bridge.controller.GameProcess;
+import bridge.domain.BridgeMaker;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        BridgeMaker bm = new BridgeMaker();
+
+        GameProcess gameProcess = new GameProcess();
+        gameProcess.start();
 
     }
 }
